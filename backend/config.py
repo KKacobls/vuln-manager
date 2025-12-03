18dafa86dc9434255aedf0a1c4dc63e9ce51704e
KKacobls <58775586+KKacobls@users.noreply.github.com>
2025-12-03 15:05:58 +0800

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -11,7 +11,7 @@ class Config:
     DB_HOST = os.environ.get('DB_HOST', 'localhost')
     DB_PORT = os.environ.get('DB_PORT', '3306')
     DB_USER = os.environ.get('DB_USER', 'test')
-    DB_PASSWORD = os.environ.get('DB_PASSWORD', 'aabb0011')
+    DB_PASSWORD = os.environ.get('DB_PASSWORD', 'aabb')
     DB_NAME = os.environ.get('DB_NAME', 'vuln_reports')
     
     @classmethod
